65f98bb9e21de5e9cfdab1cb9a913e1458833e1d
Guilherme Lira Ribeiro <gui.liraribeiro10@gmail.com>
2026-08-09 18:11:24 -0300

    [fifo.sv] (CORRECTION) write/read_ptr sizes. Full and Empty flags assigned

--- fifo.sv
+++ fifo.sv
@@ -11,7 +11,12 @@ module Sync_FIFO #(parameter DEPTH=8, parameter DATA_WIDTH=8) (
 
   parameter BIT_WIDTH = $clog2(DEPTH); // ceiling log 2
 
-  reg [BIT_WIDTH + 1:0] write_ptr;
-  reg [BIT_WIDTH + 1:0] read_ptr;
+  // One extra bit is used to differentiate
+  // FIFO EMPTY of FIFO FULL
+  reg [BIT_WIDTH:0] write_ptr;
+  reg [BIT_WIDTH:0] read_ptr;
+
+  assign full = {~write_ptr[BIT_WIDTH], write_ptr[BIT_WIDTH-1:0]} == read_ptr;
+  assign empty = write_ptr == read_ptr;
 
 endmodule: Sync_FIFO
