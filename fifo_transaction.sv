7ae21ab4908820708b34e12671e83b7bd761dcad
Guilherme Lira Ribeiro <gui.liraribeiro10@gmail.com>
2026-08-15 19:01:43 -0300

    [fifo_transaction] transaction class v1 added

---
+++ fifo_transaction.sv
@@ -0,0 +1,19 @@
+// fifo_transaction.sv
+class fifo_transaction #(parameter WIDTH = 8);
+  // Random attributes
+  rand bit wr_en,
+           rd_en;
+
+  rand bit [WIDTH-1:0] data;
+
+  // Sampling attributes
+  logic full, empty;
+
+  function bit compare(fifo_transaction tx2);
+    if (data != tx2.data) begin
+      if (tx2 == null) return 0;
+      return (this.data == tx2.data);
+    end
+  endfunction
+
+endclass: fifo_transaction
