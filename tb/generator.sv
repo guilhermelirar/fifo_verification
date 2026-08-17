bee9db9eb465f62dd509ee61ef207b0e908ffce6
Guilherme Lira Ribeiro <gui.liraribeiro10@gmail.com>
2026-08-17 16:18:50 -0300

    [generator] creating transaction items (v1)

---
+++ tb/generator.sv
@@ -0,0 +1,18 @@
+// tb/generator.sv
+// Class that generates transactions
+class Generator;
+  mailbox #(fifo_transaction) mbx_out;
+
+  function new(mailbox #(fifo_transaction) mbx_out);
+    this.mbx_out = mbx_out; // mbx to Driver
+  endfunction
+
+  task run(int n_txn);
+    repeat (n_txn) begin
+      fifo_transaction txn = new();
+      txn.randomize();
+      mbx_out.put(txn);
+    end
+  endtask
+
+endclass: Generator
