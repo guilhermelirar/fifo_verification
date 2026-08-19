e8364eaf758fe7bba16d26e7dce08732840141e5
Guilherme Lira Ribeiro <gui.liraribeiro10@gmail.com>
2026-08-19 13:09:49 -0300

    [fifo_transaction] display task

--- tb/fifo_transaction.sv
+++ tb/fifo_transaction.sv
@@ -14,4 +14,12 @@ class fifo_transaction #(parameter WIDTH = 8);
       return (this.data == tx2.data);
   endfunction
 
+  function display(string prefix = "[fifo_transaction.display]");
+    $display(prefix);
+    $display("\twr_en: %b\n\trd_en: %b\n\tfull: %b \n\tempty: %b\n\tdata: 0x%h",
+      wr_en, rd_en,
+      full, empty,
+      data);
+  endfunction
+
 endclass: fifo_transaction
