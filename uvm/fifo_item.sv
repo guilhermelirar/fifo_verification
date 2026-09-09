f2f55417058d5ee4c1b86fc481fdb36db4a7ee7c
Guilherme Lira Ribeiro <gui.liraribeiro10@gmail.com>
2026-09-09 16:32:46 -0300

    [uvm] fifo_item.sv created

--- uvm/fifo_item.sv
+++ uvm/fifo_item.sv
@@ -1,1 +1,22 @@
-// uvm_seq_item
+// uvm_seq_item for fifo transaction item
+class fifo_item #(DATA_WIDTH = 8) extends uvm_seq_item;
+  rand bit wr_en, rd_en;
+  rand bit [DATA_WIDTH-1:0] data_in;
+  
+  logic [DATA_WIDTH-1:0] data_out;
+  logic full, empty;
+  
+  // register into factory and field automation
+  `uvm_object_utils_begin(fifo_item)
+    `uvm_field_int(data_in, UVM_ALL_ON)
+    `uvm_field_int(data_out, UVM_ALL_ON)
+    `uvm_field_int(wr_en, UVM_ALL_ON)
+    `uvm_field_int(rd_en, UVM_ALL_ON)
+    `uvm_field_int(empty, UVM_ALL_ON)
+    `uvm_field_int(full, UVM_ALL_ON)
+  `uvm_object_utils_end
+
+  function new(string name = "fifo_item");
+    super.new(name);
+  endfunction
+endclass
