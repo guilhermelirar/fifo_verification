5c21d6548bcaf7db5c4b67d67028c3618afb4102
Guilherme Lira Ribeiro <gui.liraribeiro10@gmail.com>
2026-09-10 08:19:03 -0300

    [uvm sequence_item] extending from correct class fix

--- uvm/fifo_item.sv
+++ uvm/fifo_item.sv
@@ -1,11 +1,11 @@
 // uvm_seq_item for fifo transaction item
-class fifo_item #(DATA_WIDTH = 8) extends uvm_seq_item;
+class fifo_item #(DATA_WIDTH = 8) extends uvm_sequence_item;
   rand bit wr_en, rd_en;
   rand bit [DATA_WIDTH-1:0] data_in;
-  
+
   logic [DATA_WIDTH-1:0] data_out;
   logic full, empty;
-  
+
   // register into factory and field automation
   `uvm_object_utils_begin(fifo_item)
     `uvm_field_int(data_in, UVM_ALL_ON)
