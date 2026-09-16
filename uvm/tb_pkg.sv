023adeebfea94c0ddb150793bf48da48dea08ec4
Guilherme Lira Ribeiro <gui.liraribeiro10@gmail.com>
2026-09-16 14:05:53 -0300

    [uvm test_suite] specialized test init

--- uvm/tb_pkg.sv
+++ uvm/tb_pkg.sv
@@ -1,8 +1,11 @@
 package tb_pkg;
   import uvm_pkg::*;
+
+  parameter int DATA_WIDTH = 8;
+
   `include "uvm_macros.svh"
   `include "fifo_item.sv"
-  `include "fifo_sequence.sv"
+  `include "fifo_sequences.sv"
   `include "fifo_driver.sv"
   `include "fifo_coverage.sv"
   `include "fifo_monitor.sv"
--- uvm/test_suite.sv
+++ uvm/test_suite.sv
@@ -1,25 +1,31 @@
 class test_base extends uvm_test;
   `uvm_component_utils(test_base)
 
-  function new(string name, uvm_component parent);
+  function new(string name = "test_base", uvm_component parent = null);
     super.new(name, parent);
     `uvm_info("TRACE", $sformatf("%m"), UVM_HIGH);
   endfunction
 
-  function void build_phase(uvm_phase phase); endfunction
-
-  function void start_of_simulation();
+  function void start_of_simulation_phase(uvm_phase phase);
+    super.start_of_simulation_phase(phase);
     uvm_top.print_topology();
   endfunction
 
   virtual task run_phase(uvm_phase phase);
-    fifo_item item = new("fifo_item #1");
-    phase.raise_objection(this);
-    // [PLACEHOLDER] Should use sequence
-    item.print();
+    fifo_sequence#(DATA_WIDTH) seq;
+    seq = fifo_sequence#(DATA_WIDTH)::type_id::create("seq");
 
-    item.print();
+    phase.raise_objection(this);
+    // seq.start(env.sequencer);
     phase.drop_objection(this);
   endtask
-
 endclass: test_base
+
+class test_write_heavy extends test_base;
+  `uvm_component_utils(test_write_heavy)
+
+  function new(string name = "test_write_heavy", uvm_component parent);
+    super.new(name, parent);
+  endfunction
+
+endclass
