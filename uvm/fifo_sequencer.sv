7570e86aa422fea266b4b3be9ce9cce40f782738
Guilherme Lira Ribeiro <gui.liraribeiro10@gmail.com>
2026-09-16 15:49:06 -0300

    [uvm fifo sequencer] fixing type

--- uvm/fifo_sequencer.sv
+++ uvm/fifo_sequencer.sv
@@ -1,10 +1,9 @@
 // fifo_sequencer
-class fifo_sequencer extends uvm_sequencer #(fifo_item);
-  `uvm_component_utils(fifo_sequencer)
+class fifo_sequencer #(int DATA_WIDTH = 8) extends uvm_sequencer #(fifo_item #(DATA_WIDTH));
+  `uvm_component_param_utils(fifo_sequencer #(DATA_WIDTH))
 
-  function new(string name = "fifo_sequencer", uvm_component parent);
+  function new(string name = "fifo_sequencer", uvm_component parent = null);
     super.new(name, parent);
-    `uvm_info(this.get_type_name(), "%m", UVM_HIGH);
+    `uvm_info(get_type_name(), "%m Sequencer instantiated", UVM_HIGH);
   endfunction
-
 endclass
