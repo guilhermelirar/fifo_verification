eab1f1cc9a40d956b64130d0e9fc5b25d9c563c0
Guilherme Lira Ribeiro <gui.liraribeiro10@gmail.com>
2026-09-20 20:50:34 -0300

    [uvm] executable test (failing to retrieve interface)

--- uvm/test_suite.sv
+++ uvm/test_suite.sv
@@ -8,8 +8,12 @@ class test_base extends uvm_test;
     `uvm_info("TRACE","%m", UVM_HIGH);
   endfunction
 
-  function void start_of_simulation_phase(uvm_phase phase);
-    super.start_of_simulation_phase(phase);
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    m_top_env = fifo_env#(DATA_WIDTH, DEPTH)::type_id::create(
+      "m_top_env", this
+    );
+    `uvm_info(get_type_name(), "%m Test built", UVM_HIGH);
   endfunction
 
   virtual task run_phase(uvm_phase phase);
@@ -17,7 +21,7 @@ class test_base extends uvm_test;
     seq = fifo_sequence#(DATA_WIDTH)::type_id::create("seq");
 
     phase.raise_objection(this);
-    //seq.start(env.sequencer);
+    seq.start(m_top_env.m_agent.sequencer);
     phase.drop_objection(this);
   endtask
 endclass: test_base
